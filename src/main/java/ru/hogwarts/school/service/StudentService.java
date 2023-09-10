9a1bbc57da091a9333360cf99a48b01d3ff05e3c
Sergei Borisov <bmwth2311@gmail.com>
2023-09-10 15:03:39 +0300

    ДЗ

--- src/main/java/ru/hogwarts/school/service/StudentService.java
+++ src/main/java/ru/hogwarts/school/service/StudentService.java
@@ -1,2 +1,18 @@
-package ru.hogwarts.school.service;public interface StudentService {
+package ru.hogwarts.school.service;
+
+import ru.hogwarts.school.model.Student;
+
+import java.util.Collection;
+
+public interface StudentService {
+
+    Student addStudent(Student student);
+
+    Student findStudent(long id);
+
+    Student editStudent(long id, Student student);
+
+    void deleteStudent(long id);
+
+    Collection findByAge(int age);
 }
